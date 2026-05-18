d401e974f33896f7046e5c1ddcacb6b50839c77e
3emeart <3emebeats@gmail.com>
2026-05-17 23:46:20 -0300

    fix: update apiUrl in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fluxpdv.onrender.com',
+  apiUrl: 'https://fluxpdv.onrender.com/api/v1',
   authUrl: 'https://fluxpdv.onrender.com/api/auth'
 };
